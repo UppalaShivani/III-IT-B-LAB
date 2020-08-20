4d415cb448ce238ebb2dae366fd71dacdbb72fec
Uppala Shivani <69143281+UppalaShivani@users.noreply.github.com>
2020-08-20 21:07:00 +0530

    Add files via upload

---
+++ C++ Lab/Experiment 1/Arithmetic operators/Arithmetic operators.cpp
@@ -0,0 +1,16 @@
+/* 1.program on working of Arthematic operators */
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"enter two values: ";
+    cin >>x>>y;
+    cout<<"given numbers are  "<<x<<" and  "<<y<<endl;
+    cout<<"addition of given numbers is : "<<x+y << endl;
+    cout<<"subtraction of given numbers is : "<<x-y<<endl;
+    cout<<"multiplication of given numbers is : "<<x*y<<endl;
+    cout<<"dividion of given numbers is : "<<x/y<<"\n";
+    cout<<"modular division of given numbers is : "<<x%y<<endl;
+    return 0;
+}
