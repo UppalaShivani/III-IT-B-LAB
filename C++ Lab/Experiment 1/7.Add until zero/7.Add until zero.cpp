c1091e6afd2a050fe850a91885d13568d32a8201
Uppala Shivani <69143281+UppalaShivani@users.noreply.github.com>
2020-08-21 15:52:15 +0530

    Add files via upload

---
+++ C++ Lab/Experiment 1/7.Add until zero/7.Add until zero.cpp
@@ -0,0 +1,17 @@
+/*Add until zero */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,sum=0;
+    cout<<"enter number to add : ";
+    cin>>i;
+    while(i != 0)
+    {
+        cout<<"enter number to add : ";
+        cin>>i;
+        sum = sum+i;
+    }
+    cout<<"sum = "<<sum;
+    return 0;
+}
