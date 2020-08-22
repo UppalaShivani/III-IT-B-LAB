0ac21ac3e6517fd31d4e3079e60c99d534d9786c
Uppala Shivani <69143281+UppalaShivani@users.noreply.github.com>
2020-08-22 15:21:03 +0530

    Update 7.Add until zero.cpp

--- C++ Lab/Experiment 1/7.Add until zero/7.Add until zero.cpp
+++ C++ Lab/Experiment 1/7.Add until zero/7.Add until zero.cpp
@@ -4,14 +4,12 @@ using namespace std;
 int main()
 {
     int i,sum=0;
-    cout<<"enter number to add : ";
-    cin>>i;
-    while(i != 0)
+    do
     {
         cout<<"enter number to add : ";
         cin>>i;
         sum = sum+i;
-    }
+    }while(i != 0);
     cout<<"sum = "<<sum;
     return 0;
 }
