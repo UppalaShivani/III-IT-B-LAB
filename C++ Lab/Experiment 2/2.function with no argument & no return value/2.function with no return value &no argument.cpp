731829c994cf98ca5aeea086d62cfa4e1b75358e
Uppala Shivani <69143281+UppalaShivani@users.noreply.github.com>
2020-08-26 21:24:08 +0530

    Add files via upload

---
+++ C++ Lab/Experiment 2/2.function with no argument & no return value/2.function with no return value &no argument.cpp
@@ -0,0 +1,11 @@
+/* Write a C++ program for function with No arguments passed and no return value */
+#include <iostream>
+using namespace std;
+void printmessage ()
+{
+  cout << "I'm a function with no return type";
+}
+int main ()
+{
+  printmessage ();
+}
