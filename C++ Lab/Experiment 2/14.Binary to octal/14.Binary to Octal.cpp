e909e40024e05379b26ff2ee461d5a977f1a0f5e
Uppala Shivani <69143281+UppalaShivani@users.noreply.github.com>
2020-08-27 16:36:38 +0530

    Add files via upload

---
+++ C++ Lab/Experiment 2/14.Binary to octal/14.Binary to Octal.cpp
@@ -0,0 +1,55 @@
+/* Program to convert Binary to Octal and vice-versa */
+#include <iostream>
+#include <cmath>
+using namespace std;
+long long convertOctalToBinary(int octalNumber);
+int convertBinarytoOctal(long long);
+int main()
+{
+    long long binaryNumber;
+    int octalNumber;
+    cout << "Enter a binary number: ";
+    cin >> binaryNumber;
+    cout << binaryNumber << " in binary = " << convertBinarytoOctal(binaryNumber) << " in octal "<<endl;
+    cout << "Enter an octal number: ";
+    cin >> octalNumber;
+    cout << octalNumber << " in octal = " << convertOctalToBinary(octalNumber) << " in binary"<<endl;
+    return 0;
+}
+int convertBinarytoOctal(long long binaryNumber)
+{
+    int octalNumber = 0, decimalNumber = 0, i = 0;
+    while(binaryNumber != 0)
+    {
+        decimalNumber += (binaryNumber%10) * pow(2,i);
+        ++i;
+        binaryNumber/=10;
+    }
+    i = 1;
+    while (decimalNumber != 0)
+    {
+        octalNumber += (decimalNumber % 8) * i;
+        decimalNumber /= 8;
+        i *= 10;
+    }
+    return octalNumber;
+}
+long long convertOctalToBinary(int octalNumber)
+{
+    int decimalNumber = 0, i = 0;
+    long long binaryNumber = 0;
+    while(octalNumber != 0)
+    {
+        decimalNumber += (octalNumber%10) * pow(8,i);
+        ++i;
+        octalNumber/=10;
+    }
+    i = 1;
+    while (decimalNumber != 0)
+    {
+        binaryNumber += (decimalNumber % 2) * i;
+        decimalNumber /= 2;
+        i *= 10;
+    }
+    return binaryNumber;
+}
